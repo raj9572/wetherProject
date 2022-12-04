10c8b57ad5611406f2dda1a27779bd971f34c7c6
rajali <rajali1432@gmail.com>
2022-12-04 14:24:51 +0530

    adding back button

--- src/app.js
+++ src/app.js
@@ -5,38 +5,38 @@ const port = process.env.PORT || 8000;
 const hbs = require('hbs')
 
 // public static path
-const staticPath = path.join(__dirname,'../public')
+const staticPath = path.join(__dirname, '../public')
 app.use(express.static(staticPath))
 
 
 // set views as hbs
-app.set('view engine','hbs')
-const views_path = path.join(__dirname,'../templetes/views')
-app.set('views',views_path)
+app.set('view engine', 'hbs')
+const views_path = path.join(__dirname, '../templetes/views')
+app.set('views', views_path)
 // console.log(path.join(__dirname,'../templetes/views'));
 
-const partialsPath = path.join(__dirname,'../templetes/partials')
+const partialsPath = path.join(__dirname, '../templetes/partials')
 hbs.registerPartials(partialsPath)
 
 
 
 
 // routing
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index')
 })
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('about')
 })
-app.get('/weather',(req,res)=>{
+app.get('/weather', (req, res) => {
     res.render('weather')
 })
-app.get('*',(req,res)=>{
-    res.render('404error',{
-        errormsg:'Opps page not found'
+app.get('*', (req, res) => {
+    res.render('404error', {
+        errormsg: 'Opps page not found , click here to go back'
     })
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`express is running at ${port}`);
 })
